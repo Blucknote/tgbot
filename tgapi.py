eae6b9817545880ac5db4027bbd2dff6cd064c8c
Evgeniy <ccatb@mail.ru>
2018-06-18 16:18:09 +0300

    fix mistakes

--- tgapi.py
+++ tgapi.py
@@ -34,7 +34,7 @@ def set_webhook(self_domain_path: str, certificate):
     return urlopen('{api}{method}{url}{cert}'.format(**addr))
 
 def delete_webhook():
-    return urlopen('%s/deleteWebhook' % (domain % token))
+    return urlopen('%sdeleteWebhook' % (domain % token))
 
 @retry
 def send_message(chatid, msg, reply_markup = ''):    
@@ -135,7 +135,7 @@ def get_updates(offset):
 
 @retry
 def get_me():
-    return urlopen('%s/getMe' % (domain % token)).read().decode('utf-8')
+    return urlopen('%sgetMe' % (domain % token)).read().decode('utf-8')
 
 if __name__ == '__main__':
     print(get_me())
