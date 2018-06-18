d1dff6867635e84dc8e237ccea60ac3a54dfc003
Evgeniy <ccatb@mail.ru>
2018-06-18 18:27:30 +0300

    Update __init__.py
    cleaning

--- __init__.py
+++ __init__.py
@@ -111,10 +111,11 @@ def start_server(port = 9696):
 
 def start(conffile = 'conf.yml'):
     tgapi.conf = yaml.load(open(conffile,'r').read())
-    if 'webhook' in tgapi.conf.keys() or tgapi.conf['webhook'] == True:
-        response = tgapi.set_webhook('%s%s' % (conf['domain'],conf['token']), conf['ssl'])
-        print(response.read().decode('utf-8'))
-        start_server()
+    if 'webhook' in tgapi.conf.keys():
+        if tgapi.conf['webhook'] == True:
+            response = tgapi.set_webhook('%s%s' % (conf['domain'],conf['token']), conf['ssl'])
+            print(response.read().decode('utf-8'))
+            start_server()
     else:
         #polling
         tgapi.delete_webhook()
