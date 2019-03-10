140108b4c2989b00acd7cbe180d04fec558f0a8f
Blucknote <ccatb@mail.ru>
2019-03-10 18:53:50 +0300

    pyf;ake fixes

--- tgapi.py
+++ tgapi.py
@@ -6,31 +6,36 @@ import requests
 
 domain = 'https://api.telegram.org/bot%s/'
 
-conf = yaml.load(open('conf.yml','r').read())
+conf = yaml.load(open('conf.yml', 'r').read())
+
 
 def set_webhook(self_domain_path: str, certificate):
     addr = {
         'api': domain % conf['token'],
         'method': 'setWebhook?',
         'url': 'url=%s' % self_domain_path,
-        'cert': '&certificate=%s' % open(certificate, 'r'),         
+        'cert': '&certificate=%s' % open(certificate, 'r'),
     }
+
     return urlopen('{api}{method}{url}{cert}'.format(**addr))
+
 
 def delete_webhook():
     return urlopen('%sdeleteWebhook' % (domain % conf['token']))
 
-def send_message(chatid, msg, reply_markup = ''):    
+
+def send_message(chatid, msg, reply_markup=''):
     addr = {
         'api': domain % conf['token'],
         'chatid': 'sendMessage?chat_id=%s' % chatid,
         'text': '&text=%s' % quote(msg),
-        'reply': '&reply_markup=%s' % reply_markup        
+        'reply': '&reply_markup=%s' % reply_markup
     }
-    
+
     return urlopen('{api}{chatid}{text}{reply}'.format(**addr))
 
-def send_media_group(chat_id, media: list, caption = ''):
+
+def send_media_group(chat_id, media: list, caption=''):
     for i, x in enumerate(media):
         media[i] = {'type': 'photo',
                     'media': x}
@@ -41,62 +46,72 @@ def send_media_group(chat_id, media: list, caption = ''):
         'media': '&media=%s' % json.dumps(media),
         'caption': '&caption=%s' % caption
     }
+
     return urlopen('{api}{chatid}{media}{caption}'.format(**addr))
 
-def send_photo(chat_id, photo, caption = '', reply = ''):
+
+def send_photo(chat_id, photo, caption='', reply=''):
     addr = {
         'api': domain % conf['token'],
         'method': 'sendPhoto?',
         'channel': 'chat_id=%s' % chat_id,
         'type': '&photo=%s' % photo,
         'caption': '&caption=%s' % caption,
-        'reply': '&reply_markup=%s' % reply            
+        'reply': '&reply_markup=%s' % reply
     }
+
     if isinstance(photo, bytes):
         requests.post(
-            '{api}{method}{channel}'.format(**addr), files = {'photo': photo}
+            '{api}{method}{channel}'.format(**addr), files={'photo': photo}
         )
     else:
         return urlopen(
             '{api}{method}{channel}{type}{caption}{reply}'.format(**addr)
         )
-def send_video(chat_id, video, caption = '', reply = ''):
+
+
+def send_video(chat_id, video, caption='', reply=''):
     addr = {
         'api': domain % conf['token'],
         'method': 'sendVideo?',
         'channel': 'chat_id=%s' % chat_id,
         'type': '&video=%s' % video,
         'caption': '&caption=%s' % caption,
-        'reply': '&reply_markup=%s' % reply            
+        'reply': '&reply_markup=%s' % reply
     }
+
     return urlopen(
         '{api}{method}{channel}{type}{caption}{reply}'.format(**addr)
-    )    
+    )
 
-def send_document(chat_id, document, caption = '', reply = ''):
+
+def send_document(chat_id, document, caption='', reply=''):
     addr = {
         'api': domain % conf['token'],
         'method': 'sendDocument?',
         'channel': 'chat_id=%s' % chat_id,
         'document': '&document=%s' % document,
         'caption': '&caption=%s' % caption,
-        'reply': '&reply_markup=%s' % reply            
+        'reply': '&reply_markup=%s' % reply
     }
+
     return urlopen(
         '{api}{method}{channel}{document}{caption}{reply}'.format(**addr)
     )    
+
 
 def delete_message(chatid, messageid):
     addr = {
         'api': domain % conf['token'],
         'method': 'deleteMessage?',
         'chatid': 'chat_id=%s' % chatid,
-        'msgid': '&message_id=%s' % messageid,        
+        'msgid': '&message_id=%s' % messageid,
     }
-    return urlopen('{api}{method}{chatid}{msgid}'.format(**addr))    
+    
+    return urlopen('{api}{method}{chatid}{msgid}'.format(**addr))
 
-def addr_callback(callback, text = '', alert = False,
-                    url = '', cache_time = 15):
+
+def addr_callback(callback, text='', alert=False, url='', cache_time=15):
     addr = {
         'api': domain % conf['token'],
         'chatid': 'addrCallbackQuery?callback_query_id=%s'
@@ -106,18 +121,20 @@ def addr_callback(callback, text = '', alert = False,
         'url': '&url=%s' % url,
         'cache': '&cache_time=%s' % cache_time,         
     }
-    
+
     return urlopen('{api}{chatid}{text}{alert}{url}{cache}'.format(**addr))
+
 
 def get_updates(offset):
     return urlopen(
         domain % conf['token'] + 'getUpdates?offset=%s' % offset
     ).read().decode('utf-8')
 
+
 def get_me():
     return urlopen(domain % conf['token'] + 'getMe').read().decode('utf-8')
 
 
 if __name__ == '__main__':
-    #print(get_me())
-    fire.Fire()
+    print(get_me())
+    #fire.Fire()
