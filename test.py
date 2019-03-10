f54891387abc86635ce0f664da7e39f87ca3d0df
Blucknote <ccatb@mail.ru>
2019-03-10 21:40:11 +0300

    fix

--- test.py
+++ test.py
@@ -2,27 +2,27 @@ import tgapi
 
 #assert tgapi.set_webhook()
 f=tgapi.delete_webhook()
-assert f['result'] == True, "Webhook fail, %s" % f
+assert f['ok'] == True, "Webhook fail, %s" % f
 
 f=tgapi.send_message(106989752, 'test')
-assert f['result'] == True, "sending message fail, %s" % f
+assert f['ok'] == True, "sending message fail, %s" % f
 
-#assert tgapi.send_media_group(106989752,'test')['result'] == True, "Webhook fail"
+#assert tgapi.send_media_group(106989752,'test')['ok'] == True, "Webhook fail"
 
 img = open('test.jpg', 'rb')
 f=tgapi.send_photo(106989752, img.read(), 'test')
-assert f['result'] == True, "sending image fail, %s" % f
+assert f['ok'] == True, "sending image fail, %s" % f
 img.close()
 
 vid = open('test.mp4', 'rb')
 f=tgapi.send_photo(106989752, vid.read(), 'test')
-assert f['result'] == True, "sending video fail, %s" % f
+assert f['ok'] == True, "sending video fail, %s" % f
 vid.close()
 
 
 doc = open('test.pdf', 'rb')
 f=tgapi.send_document(106989752, doc.read(), 'test')
-assert f['result'] == True, "sending document fail, %s" % f
+assert f['ok'] == True, "sending document fail, %s" % f
 doc.close()
 
 #assert tgapi.delete_message()
@@ -30,7 +30,7 @@ doc.close()
 #assert tgapi.addr_callback()
 
 f=tgapi.get_updates(0)
-assert f['result'] == True, "retrieve updates fail, %s" % f
+assert f['ok'] == True, "retrieve updates fail, %s" % f
 
 f=tgapi.get_me()
-assert f['result'] == True, "get bot info fail, %s" % f
+assert f['ok'] == True, "get bot info fail, %s" % f
