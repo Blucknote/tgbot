a4a811bf9d50aa3b8ddb686c1d9c78c2bef9b822
Blucknote <ccatb@mail.ru>
2019-03-10 21:29:15 +0300

    test fix

--- test.py
+++ test.py
@@ -1,29 +1,36 @@
 import tgapi
 
 #assert tgapi.set_webhook()
+f=tgapi.delete_webhook()
+assert f['error_code'] == 200, "Webhook fail"
 
-assert tgapi.delete_webhook()['error_code'] == 200, "Webhook fail"
-
-assert tgapi.send_message(106989752, 'test')['error_code'] == 200, "sending message fail"
+f=tgapi.send_message(106989752, 'test')
+assert f['error_code'] == 200, "sending message fail"
 
 #assert tgapi.send_media_group(106989752,'test')['error_code'] == 200, "Webhook fail"
 
-img = open('test.jpg', 'rb').read()
-assert tgapi.send_photo(106989752, img, 'test')['error_code'] == 200, "sending image fail"
+img = open('test.jpg', 'rb')
+f=tgapi.send_photo(106989752, img.read(), 'test')
+assert f['error_code'] == 200, "sending image fail"
 img.close()
 
-vid = open('test.mp4', 'rb').read()
-assert tgapi.send_photo(106989752, vid, 'test')['error_code'] == 200, "sending video fail"
+vid = open('test.mp4', 'rb')
+f=tgapi.send_photo(106989752, vid.read(), 'test')
+assert f['error_code'] == 200, "sending video fail"
 vid.close()
 
-doc = open('test.pdf', 'rb').read()
-assert tgapi.send_document(106989752, doc, 'test')['error_code'] == 200, "sending document fail"
+
+doc = open('test.pdf', 'rb')
+f=tgapi.send_document(106989752, doc.read(), 'test')
+assert f['error_code'] == 200, "sending document fail"
 doc.close()
 
 #assert tgapi.delete_message()
 
 #assert tgapi.addr_callback()
 
-assert tgapi.get_updates(0)['error_code'] == 200, "retrieve updates fail"
+f=tgapi.get_updates(0)
+assert f['error_code'] == 200, "retrieve updates fail"
 
-assert tgapi.get_me()['error_code'] == 200, "get bot info fail"
+f=tgapi.get_me()
+assert f['error_code'] == 200, "get bot info fail"
