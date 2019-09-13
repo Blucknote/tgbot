2b1aa915b2bb75f5c84e30e16b145c625623d5eb
Evgeniy <ccatb@mail.ru>
2019-09-13 14:32:37 +0300

    Update and rename events to events.py

---
+++ events.py
@@ -0,0 +1,26 @@
+def on_message(text):
+    def wrapper(fn):
+        def inner(message):
+            if re.findall(text, message['text']):
+                return(fn(message))
+        return inner
+    return wrapper
+
+
+def on_callback(data):
+    def wrapper(fn):
+        def inner(callback):
+            if re.findall(data, callback['data']):
+                return(fn(callback))
+        return inner
+    return wrapper
+
+
+def on_media(message):
+    raise NotImplementedError("Not implemented")
+  
+def on_document(document):
+    raise NotImplementedError("Not implemented")
+
+def on_sticker(document):
+    raise NotImplementedError("Not implemented")
